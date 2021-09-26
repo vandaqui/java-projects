297a17803a4df6e7dd0917a48fd301a13feb9ab4
Vandaqui-windows-main <vandicleydson@gmail.com>
2021-09-26 15:34:35 -0300

    Select department

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 	private String firstName;
 	private String lastName;
@@ -14,8 +16,27 @@ public class Email {
 		this.lastName = lastName;
 		
 		System.out.println("Email Created:" + this.firstName + " " + this.lastName);
+		
+		// Call a method asking for the department - return the department
+		this.department = setDepartment();
+		System.out.println("Department: " + this.department);
 	}
 	// Ask for the department
+	private String setDepartment() {
+		System.out.println("DEPARTMENT CODE\n1 for Sales \n2 for Development\n3 for Accounting\n0 for None\nEnter department code:");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if (depChoice == 1) {
+			return "sales";
+		} else if(depChoice == 2) {
+			return "dev";
+		} else if(depChoice == 3) {
+			return "acct";
+		} else {
+			return "";
+		}
+		
+	}
 	
 	// Generate a random password
 	
