25b000fce649c710de2ba604cd45962586db0557
Vandaqui-windows-main <vandicleydson@gmail.com>
2021-09-26 16:42:39 -0300

    Show info

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,7 +8,7 @@ public class Email {
 	private String password;
 	private String department;
 	private String email;
-	private int mailboxCapacity;
+	private int mailboxCapacity = 500;
 	private int defaultPasswordLength = 10;
 	private String alternateEmail;
 	private String companySuffix = "aeycompany.com";
@@ -18,23 +18,24 @@ public class Email {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		
-		System.out.println("Email Created:" + this.firstName + " " + this.lastName);
+		//System.out.println("Email Created:" + this.firstName + " " + this.lastName);
 		
 		// Call a method asking for the department - return the department
 		this.department = setDepartment();
-		System.out.println("Department: " + this.department);
+		//System.out.println("Department: " + this.department);
 		
 		// Call a method that returns a random password
 		this.password = randomPassword(defaultPasswordLength);
-		System.out.println("Your password is: " + this.password);
+		//System.out.println("Your password is: " + this.password);
 		
 		//Combine elements to generate email
 		email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
-		System.out.println("Your email is: " + email);
+		//System.out.println("Your email is: " + email);
 	}
+	
 	// Ask for the department
 	private String setDepartment() {
-		System.out.println("DEPARTMENT CODE\n1 for Sales \n2 for Development\n3 for Accounting\n0 for None\nEnter department code:");
+		System.out.println("New Worker: " + firstName + ". \nDepartment Codes:\n1 for Sales \n2 for Development\n3 for Accounting\n0 for None\nEnter department code:");
 		Scanner in = new Scanner(System.in);
 		int depChoice = in.nextInt();
 		if (depChoice == 1) {
@@ -46,7 +47,6 @@ public class Email {
 		} else {
 			return "";
 		}
-		
 	}
 	
 	// Generate a random password
@@ -61,8 +61,32 @@ public class Email {
 	}
 	
 	// Set the mailbox capacity
+	public void setMailboxCapacity(int capacity) {
+		this.mailboxCapacity = capacity;
+	}
 	
 	// Set the alternate email
+	public void setAlternativeEmail(String altEmail) {
+		this.alternateEmail = altEmail;
+	}
 	
 	// Change the password
+	public void changePassword(String password) {
+		this.password = password;	
+	}
+	public int getMailboxCapacity(){
+		return mailboxCapacity;
+	}
+	public String getAlternateEmail() {
+		return alternateEmail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	
+	public String showInfo() {
+		return "DISPLAY NAME: " + firstName + " " + lastName +
+				"\nCOMPANY EMAIL " + email +
+				"\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";			
+	}
 }
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -4,6 +4,8 @@ public class EmailApp {
 
 	public static void main(String[] args) {
 		Email em1 = new Email("Vand", "Aquino");
+		
+		System.out.println(em1.showInfo());
 	}
 
 }
