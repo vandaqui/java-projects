6ed25096c2aa39cd3c5bcae4616673318c181e9f
Vandaqui-windows-main <vandicleydson@gmail.com>
2021-09-30 08:33:18 -0300

    Add update student grades and update fees

--- school-manegement/src/schoolManagement/Student.java
+++ school-manegement/src/schoolManagement/Student.java
@@ -1,5 +1,42 @@
 package schoolManagement;
 
-public class Student {
+// Class responsible for keeping the track of the students info
+// These info are fees, name, grades etc.
+// The anual fee is 30k, there is no need to pay upfront
 
+public class Student {
+	
+	private int id;
+	private String name;
+	private int grade;
+	private int feesPaid;
+	private int feesTotal;
+	
+	//Create new student object
+	public Student (int id, String name, int grade) {
+		
+		this.feesPaid = 0;
+		this.feesTotal = 30000;
+		this.id = id;
+		this.name = name;
+		this.grade = grade;
+		
+	}
+	
+	// Update student grade
+	public void setGrade(int grade) {
+		
+		this.grade = grade;
+		
+	}
+	
+	
+	// Update fees paid
+	public void updateFeesPaid(int fees) {
+		feesPaid += fees;
+	}
+	
+	
+	
+	
 }
