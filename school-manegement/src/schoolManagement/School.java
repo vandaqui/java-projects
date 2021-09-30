00c9801fc8346de0b2ed993422aff0d984bd8e29
Vandaqui-windows-main <vandicleydson@gmail.com>
2021-09-30 08:52:46 -0300

    Create school class

---
+++ school-manegement/src/schoolManagement/School.java
@@ -0,0 +1,13 @@
+package schoolManagement;
+
+// High number of students and teachers
+// Implements teachers and students using ArrayList
+public class School {
+
+	private Teacher teacher;
+	private Student student;
+	private int totalMoneyEarned;
+	private int totalMoneySpent;
+	
+	
+}
