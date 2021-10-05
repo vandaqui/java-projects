0b1dbf6c94b30f571e4321ba21c5b5ee1065badf
Vand Aquino <64815489+vandaqui@users.noreply.github.com>
2021-10-05 14:20:12 -0300

    Create StringsComparison.java

---
+++ 1-useful/StringsComparison.java
@@ -0,0 +1,16 @@
+package stringTypes;
+
+public class StrinigComparison {
+
+	public static void main(String[] args) {
+
+		System.out.println("\n==== String Comparison ====\n");
+		
+		String s2 = " hello ";
+		String s = " hello ";
+		
+		
+		
+	}
+
+}
