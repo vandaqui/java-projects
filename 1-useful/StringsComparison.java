7ecb9e97d3414ced671a63389608f15a6b2494ed
Vand Aquino <64815489+vandaqui@users.noreply.github.com>
2021-10-05 14:47:04 -0300

    Update StringsComparison.java

--- 1-useful/StringsComparison.java
+++ 1-useful/StringsComparison.java
@@ -21,11 +21,15 @@ public class StrinigComparison {
 		// Puts the s3 in the pool and return the reference in the string pool
 		System.out.println("s == s3.intern(): " + (s == s3.intern()));	// true
 		
-
-
-		
+		String firstName = "Andrii";
+		String firstName2 = "andrii";
+		// Capital "A" returns false
+		System.out.println("firstName.equals(firstName2): " 
+				+ firstName.equals(firstName2)); // false
+		// Ignore case sensitivity
+		System.out.println("firstName.equalsIgnoreCase(firstName2): " 
+				+ firstName.equalsIgnoreCase(firstName2)); 	// true
 	
-		
 	}
 
 }
