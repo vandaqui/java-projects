f246894736066920e8f0675c36f838bc403c970f
Vand Aquino <64815489+vandaqui@users.noreply.github.com>
2021-10-05 13:36:33 -0300

    Create strings.java

---
+++ 1-useful/strings.java
@@ -0,0 +1,15 @@
+package stringTypes;
+
+public class strings {
+
+	public static void main(String[] args) {
+
+		System.out.println("\n====String Methods====\n\n");
+		
+		String s = "Hello";
+		
+		System.out.println("s.length(): " + s.length());
+		
+	}
+
+}
